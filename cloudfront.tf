f76ea8b10aecdd0cbd5774e841daee9e14787669
Sorah Fukumori <her@sorah.jp>
2024-06-17 05:14:24 +0900

    stop unintentional Host header forwarding to S3

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,17 +1,38 @@
-data "aws_cloudfront_origin_request_policy" "Managed-AllViewerExceptHostHeader" {
-  name = "Managed-AllViewerExceptHostHeader"
-}
 data "aws_cloudfront_origin_request_policy" "Managed-CORS-S3Origin" {
   name = "Managed-CORS-S3Origin"
 }
 data "aws_cloudfront_cache_policy" "Managed-CachingDisabled" {
   name = "Managed-CachingDisabled"
 }
-#data "aws_cloudfront_cache_policy" "Managed-UseOriginCacheControlHeaders" {
-#  name = "Managed-UseOriginCacheControlHeaders"
-#}
+
 data "aws_cloudfront_cache_policy" "Managed-CachingOptimized" {
   name = "Managed-CachingOptimized"
+}
+
+# NOTE: UseOriginCacheControlHeaders includes Host header forwarding which we don't want to.
+#data "aws_cloudfront_cache_policy" "UseOriginCacheControlHeaders" {
+#  name = "UseOriginCacheControlHeaders"
+#}
+resource "aws_cloudfront_cache_policy" "data" {
+  name        = "${var.name_prefix}-data"
+  comment     = "${var.name_prefix} CachingOptimized + min=0"
+  default_ttl = 0
+  max_ttl     = 31536000 # 1 yr
+  min_ttl     = 0
+  parameters_in_cache_key_and_forwarded_to_origin {
+    enable_accept_encoding_brotli = true
+    enable_accept_encoding_gzip   = true
+
+    cookies_config {
+      cookie_behavior = "none"
+    }
+    headers_config {
+      header_behavior = "none"
+    }
+    query_strings_config {
+      query_string_behavior = "none"
+    }
+  }
 }
 
 resource "aws_cloudfront_distribution" "public" {
@@ -37,6 +58,7 @@ resource "aws_cloudfront_distribution" "public" {
     domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
     origin_path = "/ui"
   }
+
   origin {
     origin_id   = "s3public-data"
     domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
@@ -51,7 +73,7 @@ resource "aws_cloudfront_distribution" "public" {
     cached_methods  = ["GET", "HEAD"]
 
     target_origin_id         = "s3public-data"
-    cache_policy_id          = "83da9c7e-98b4-4e11-a168-04f0df8e2c65" # data.aws_cloudfront_cache_policy.Managed-UseOriginCacheControlHeaders.id
+    cache_policy_id          = aws_cloudfront_cache_policy.data.id
     origin_request_policy_id = data.aws_cloudfront_origin_request_policy.Managed-CORS-S3Origin.id
 
     compress               = true

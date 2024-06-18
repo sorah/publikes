31031ccfd2a0635d8ff8441cdedc07eef0db4636
Sorah Fukumori <her@sorah.jp>
2024-06-19 04:58:35 +0900

    export lambda_function_url

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "cloudfront_distribution_domain_name" {
   value = aws_cloudfront_distribution.public.domain_name
 }
+
+output "lambda_function_url" {
+  value = aws_lambda_function_url.collector-http.function_url
+}
